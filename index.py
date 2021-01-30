c8bb56074c6865451f346f06a27cafc125e9d43f
Ian de Jesus <ian@eskwelabs.com>
2021-01-30 09:03:49 +0900

    added basic backtesting us backtesting.py

--- index.py
+++ index.py
@@ -4,8 +4,10 @@ from pyti.simple_moving_average import  simple_moving_average as sma
 import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
-
 from pyti.exponential_moving_average import exponential_moving_average as ema
+from backtesting import Strategy, Backtest
+from backtesting.lib import resample_apply
+from backtesting.lib import crossover
 
 
 # display data on the MetaTrader 5 package
@@ -39,35 +41,79 @@ mt5.shutdown()
 print("Display obtained data 'as is'")
 close_array = []
 for rate in rates:
-    print(rate)
-    print(type(rate))
     close_array.append(rate[4])
 
-print(close_array)
+# print(close_array)
 # create DataFrame out of the obtained data
 rates_frame = pd.DataFrame(rates)
 # convert time in seconds into the datetime format
-rates_frame['time']=pd.to_datetime(rates_frame['time'], unit='s')
+# print(rates_frame)
+# rates_frame['time']=pd.to_datetime(rates_frame['time'])
                            
 # display data
 
 
-period = 15
-res = ema(close_array, period)
-print(res)
-rates_frame.insert(5, "15ema", res, True)
+# period = 15
+# res = ema(close_array, period)
+# print(res)
+# rates_frame.insert(5, "15ema", res, True)
 
-period = 200
-res = ema(close_array, period)
-rates_frame.insert(6, "200ema", res, True)
+# period = 200
+# res = ema(close_array, period)
+# rates_frame.insert(6, "200ema", res, True)
 
-print("\nDisplay dataframe with data")
+print("\nDisplay dataframe with data a")
+rates_frame.set_index('time', inplace=True)
+rates_frame.rename(columns={'open': 'Open', 'high': 'High', 'low': 'Low', 'close': 'Close'}, inplace=True)
+
+# rates_frame.columns['Open', 'High','Low', 'Close']
+del rates_frame['tick_volume']
+del rates_frame['spread']
+del rates_frame['real_volume']
+
 print(rates_frame)  
 
-plt.plot(rates_frame['time'], rates_frame['close'], 'r-', label='close')
-plt.plot(rates_frame['time'], rates_frame['15ema'], 'b-', label='15ema')
-plt.plot(rates_frame['time'], rates_frame['200ema'], 'g-', label='200ema')
+# plt.plot(rates_frame['time'], rates_frame['close'], 'r-', label='close')
+# plt.plot(rates_frame['time'], rates_frame['15ema'], 'b-', label='15ema')
+# plt.plot(rates_frame['time'], rates_frame['200ema'], 'g-', label='200ema')
 
 
-plt.show()
+# plt.show()
 
+def SMA(values, n):
+    """
+    Return simple moving average of `values`, at
+    each step taking into account `n` previous values.
+    """
+    return pd.Series(values).rolling(n).mean()
+
+class SmaCross(Strategy):
+    # Define the two MA lags as *class variables*
+    # for later optimization
+    n1 = 10
+    n2 = 20
+    
+    def init(self):
+        # Precompute the two moving averages
+        self.sma1 = self.I(SMA, self.data.Close, self.n1)
+        self.sma2 = self.I(SMA, self.data.Close, self.n2)
+    
+    def next(self):
+        # If sma1 crosses above sma2, close any existing
+        # short trades, and buy the asset
+        if crossover(self.sma1, self.sma2):
+            self.position.close()
+            self.buy()
+
+        # Else, if sma1 crosses below sma2, close any existing
+        # long trades, and sell the asset
+        elif crossover(self.sma2, self.sma1):
+            self.position.close()
+            self.sell()
+
+
+bt = Backtest(rates_frame, SmaCross, cash=10_000, commission=.002)
+stats = bt.run()
+print(stats)
+bt.run()
+bt.plot()
