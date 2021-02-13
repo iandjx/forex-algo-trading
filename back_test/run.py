5d154254613a350cc7b7b7bf122ba27195db254e
Ian de Jesus <ian@eskwelabs.com>
2021-02-13 20:15:47 +0900

    fetch fxcm data

---
+++ back_test/run.py
@@ -0,0 +1,13 @@
+import fxcmpy
+import datetime as dt
+
+con = fxcmpy.fxcmpy(config_file='../fxcm.cfg', server='demo')
+
+start = dt.datetime(2020, 1, 1)
+stop = dt.datetime(2021, 1, 21)
+
+data = con.get_candles('EUR/USD', period='H4', start=start, end=stop)
+
+print(data)
+
+con.close()
