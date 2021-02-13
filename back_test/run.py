a6d564535b34f6376e2f5d4a9d82551f1381d8db
Ian de Jesus <ian@eskwelabs.com>
2021-02-13 21:06:20 +0900

    modify column headers

--- back_test/run.py
+++ back_test/run.py
@@ -8,6 +8,9 @@ stop = dt.datetime(2021, 1, 21)
 
 data = con.get_candles('EUR/USD', period='H4', start=start, end=stop)
 
+data.rename(columns={'askopen': 'Open', 'askhigh': 'High',
+                     'bidlow': 'Low', 'bidclose': 'Close'}, inplace=True)
+
 print(data)
 
 con.close()
