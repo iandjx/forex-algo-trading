2442f680ddffe74fc0c1f857fcea6e6c8016f3f5
Ian de Jesus <ian@eskwelabs.com>
2021-02-16 11:28:26 +0900

    updated step.py

--- sandbox/step.py
+++ sandbox/step.py
@@ -2,8 +2,21 @@ import subprocess
 import sys
 
 commits = [
-    "asdasd",
-    "asdasdasd"
+    "5d15425",
+    "a6d5645",
+    "c04927d",
+    "2f5246b",
+    "8d4cbbe",
+    "2c70220",
+    "d725c31",
+    "408ea8e",
+    "da0f695",
+    "cc53756",
+    "e974c9f",
+    "3666a44",
+    "0b907b1",
+    "7fdd9b3",
+    "bc34920"
 ]
 
 step = sys.argv[1]
