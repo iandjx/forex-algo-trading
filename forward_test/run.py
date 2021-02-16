2c702205034d5ab5c527bfca2982235819bd8864
Ian de Jesus <ian@eskwelabs.com>
2021-02-16 10:23:24 +0900

    add prepare function for forward testing

--- forward_test/run.py
+++ forward_test/run.py
@@ -1,10 +1,26 @@
 import fxcmpy
+import time
+import datetime as dt
+
+
+###### USER PARAMETERS ######
+token = 'INSERT-TOKEN-HERE'
+symbol = 'GBP/USD'
+timeframe = "m1"                    # (m1,m5,m15,m30,H1,H2,H3,H4,H6,H8,D1,W1,M1)
+#############################
+
+# Global Variables
+pricedata = None
+numberofcandles = 300
 
 
 con = fxcmpy.fxcmpy(config_file='../fxcm.cfg', server='demo')
 
-con.subscribe_market_data('EUR/USD')
+# This function runs once at the beginning of the strategy to run initial one-time processes
+def Prepare():
+    global pricedata
 
-data = con.get_prices('EUR/USD')
-
-print(data)
+    print("Requesting Initial Price Data...")
+    pricedata = con.get_candles(symbol, period=timeframe, number=numberofcandles)
+    print(pricedata)
+    print("Initial Price Data Received...")
